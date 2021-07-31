2501ec6eec4c6195eb76d106fba849b3b4027dde
Aylinux <cihanalk@gmail.com>
2021-07-31 10:54:13 +0300

    Create time-shutdown_tr_TR.ts (#1)

---
+++ translations/time-shutdown_tr_TR.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/comboxwidget.cpp" line="19"/>
+        <source>Shutdown</source>
+        <translation>Bilgisayarı Kapat</translation>
+    </message>
+    <message>
+        <location filename="../src/confirmareawidget.cpp" line="14"/>
+        <source>Cancel</source>
+        <translation>İptal</translation>
+    </message>
+    <message>
+        <location filename="../src/confirmareawidget.cpp" line="17"/>
+        <source>Confirm</source>
+        <translation>Onayla</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="66"/>
+        <source>never</source>
+        <translation>Asla</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="67"/>
+        <source>Only this shutdown</source>
+        <translation>Sadece bu kapatma</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="68"/>
+        <source>Everyday</source>
+        <translation>Her gün</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="69"/>
+        <source>Mon</source>
+        <translation>Pzt</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="70"/>
+        <source>Tue</source>
+        <translation>Sal</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="71"/>
+        <source>Wed</source>
+        <translation>Çar</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="72"/>
+        <source>Thu</source>
+        <translation>Per</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="73"/>
+        <source>Fri</source>
+        <translation>Cum</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="74"/>
+        <source>Sat</source>
+        <translation>Cts</translation>
+    </message>
+    <message>
+        <location filename="../src/dropdownbox.cpp" line="75"/>
+        <source>Sun</source>
+        <translation>Paz</translation>
+    </message>
+    <message>
+        <location filename="../src/timeshowwidget.cpp" line="39"/>
+        <source>hours</source>
+        <translation>Saat</translation>
+    </message>
+    <message>
+        <location filename="../src/timeshowwidget.cpp" line="43"/>
+        <location filename="../src/widget.cpp" line="149"/>
+        <source>minute</source>
+        <translation>Dakika</translation>
+    </message>
+    <message>
+        <location filename="../src/widget.cpp" line="24"/>
+        <source>time-shutdown</source>
+        <translation>Kapatma zamanı</translation>
+    </message>
+    <message>
+        <location filename="../src/widget.cpp" line="147"/>
+        <source>next shutdown</source>
+        <translation>Sonraki kapatma</translation>
+    </message>
+    <message>
+        <location filename="../src/widget.cpp" line="148"/>
+        <source>hour</source>
+        <translation>Saat</translation>
+    </message>
+    <message>
+        <source>minutes</source>
+        <translation type="vanished">Dakika</translation>
+    </message>
+</context>
+<context>
+    <name>VerticalScroll_60</name>
+    <message>
+        <location filename="../src/verticalscroll60.cpp" line="243"/>
+        <source>VerticalScroll_60</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../src/widget.cpp" line="237"/>
+        <source>Mi&amp;nimize</source>
+        <translation>Aşağı İndir</translation>
+    </message>
+    <message>
+        <location filename="../src/widget.cpp" line="240"/>
+        <source>&amp;Restore</source>
+        <translation>Onar</translation>
+    </message>
+    <message>
+        <location filename="../src/widget.cpp" line="243"/>
+        <source>&amp;Quit</source>
+        <translation>Çıkış</translation>
+    </message>
+</context>
+</TS>
